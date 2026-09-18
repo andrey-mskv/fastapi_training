3006f15b94050e200eb2b16c4bde387d23c199cf
Andrei Moskvin <andrey.mskv@gmail.com>
2026-09-18 07:04:14 +0400

    enum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from enum import StrEnum
 
 # import datetime
 
@@ -7,39 +8,41 @@ import uvicorn
 app = FastAPI()  # (docs_url=None, redoc_url=None) для отключения документации
 
 
-# Новый эндпоинт: приветствие для автора.
+class EducationLevel(StrEnum):
+    SECONDARY = 'Среднее образование'
+    SPECIAL = 'Среднее специальное образование'
+    HIGHER = 'Высшее образование'
+
+
 @app.get('/me')
 async def hello_author():
     return {'Hello': 'dear author'}
 
 
-# Декоратор, определяющий, что GET-запросы к основному URL приложения
-# должны обрабатываться этой функцией.
 @app.get('/{name}')
 async def greetings(
     name: str,
     surname: str,
     age: int | None = None,
     is_staff: bool = False,
+    # Добавляем новый параметр: образование.
+    education_level: EducationLevel | None = None,
 ) -> dict[str, str]:
-    # Объединяем имя и фамилию в единую строку:
     result = ' '.join([name, surname])
-    # Выводим все слова с заглавной буквы:
     result = result.title()
     if age is not None:
-        # Добавляем к выводу возраст (если есть);
-        # приводим число age к строке.
         result += ', ' + str(age)
-    # Если is_staff равен True...
+
+    # Добавляем проверку — передан ли параметр education_level:
+    if education_level is not None:
+        # Чтобы текст смотрелся грамотно,
+        # переведём строку education_level в нижний регистр.
+        result += ', ' + education_level.lower()
+
     if is_staff:
-        # ...дописываем слово "сотрудник".
         result += ', сотрудник'
     return {'Hello': result}
 
 
-# А почему async? А потому что мы пишем асинхронное приложение!
-# Все обработчики запросов будем объявлять через async def.
 if __name__ == '__main__':
-    # Команда на запуск uvicorn. При необходимости здесь же можно указать
-    #  хост, порт и другие параметры сервера.
     uvicorn.run('main:app', reload=True)
