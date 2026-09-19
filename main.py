bed4592a8ccdbe424b760ec4e679caa003d1c4d9
Andrei Moskvin <andrey.mskv@gmail.com>
2026-09-19 12:10:38 +0400

    path/query validation

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from fastapi import FastAPI
-import uvicorn
+from fastapi import FastAPI, Path, Query
 from enum import StrEnum
-from random import randint, choice
-from secrets import token_hex
-from uuid import uuid4
 
-# import datetime
-
-# Создание объекта приложения.
-app = FastAPI()  # (docs_url=None, redoc_url=None) для отключения документации
+app = FastAPI()
 
 
 class EducationLevel(StrEnum):
@@ -26,13 +19,13 @@ async def hello_author():
     '/{name}',
     tags=['common methods'],
     summary='Общее приветствие',
-    # description='Приветствие человека по имени и фамилии; '
-    # 'опционально указывается возраст, образование и статус сотрудника',
     response_description='Полная строка приветствия',
 )
 async def greetings(
-    name: str,
-    surname: str,
+    # Устанавливаем ограничение для path-параметра:
+    name: str = Path(min_length=2, max_length=20),
+    # Устанавливаем ограничение для query-параметра:
+    surname: str = Query(min_length=2, max_length=50),
     age: int | None = None,
     is_staff: bool = False,
     education_level: EducationLevel | None = None,
@@ -57,84 +50,3 @@ async def greetings(
     if is_staff:
         result += ', сотрудник'
     return {'Hello': result}
-
-
-class Tag(StrEnum):
-    FRONT = 'Для фронтендера'
-    BACK = 'Для бэкендера'
-    ALL = 'Для всех'
-
-
-@app.get(
-    '/hexcolor',
-    tags=[Tag.FRONT],
-    summary='Случайный hex',
-    description='Возвращает код цвета в формате HEX',
-    response_description='Полная строка приветствия',
-)
-async def get_hex_color() -> str:
-    return f'#{token_hex(3)}'
-
-
-@app.get(
-    '/uuid',
-    tags=[Tag.BACK],
-    summary='Рандомный uuid',
-    description='Возвращает случайный уникальный идентификатор UUIDv4',
-    response_description='Полная строка приветствия',
-)
-async def get_random_uuid() -> str:
-    return str(uuid4())
-
-
-HELLO_WORLD_LIST = [
-    'Hello world',
-    'Hola mundo',
-    'Bonjour le monde',
-    'Hallo Welt',
-    'Ciao mondo',
-    'Olá mundo',
-    'Привет мир',
-    'こんにちは世界',
-    '你好世界',
-    'مرحبا بالعالم',
-]
-
-
-@app.get(
-    '/helloworld',
-    tags=[Tag.BACK],
-    summary='multilangeage',
-    description='Возвращает фразу Hello world на случайно выбранном языке',
-    response_description='Полная строка приветствия',
-)
-async def get_hello_world() -> str:
-    return choice(HELLO_WORLD_LIST)
-
-
-@app.get(
-    '/dumplings',
-    tags=[Tag.ALL],
-    summary='Пельмени',
-    description='Возвращает количество пельменей,'
-    'которые следует сварить для поддержания сил (в диапазоне от 5 до 20)',
-    response_description='Полная строка приветствия',
-)
-async def get_dumplings_count() -> int:
-    return randint(5, 20)
-
-
-@app.get(
-    '/coin',
-    tags=[Tag.ALL],
-    summary='Красное&Белое',
-    description='Помогает принять важное решение:'
-    'подбрасывает виртуальную монетку и возвращает True или False',
-    response_description='Полная строка приветствия',
-)
-async def toss_a_coin() -> bool:
-    return choice([True, False])
-
-
-if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True)
